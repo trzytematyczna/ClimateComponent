25ca52cfb1da248c73d3ceec469113c9e972fcc3
trzytematyczna <monikrakoczy@gmail.com>
2020-12-09 14:35:25 +0100

    path for texts function change to ./data/content/

--- src/R/event-analyzer.R
+++ src/R/event-analyzer.R
@@ -256,8 +256,7 @@ texts<-function(ids, corpus = "guardian"){
    
   # x<-paste0("xargs -I {} grep \"^{}\" data/",corpus,"-all-data.csv < ", ids$doc_ids)
   # cat(x)
-  xcommand<-paste0('grep -P \"', ids_string,'\" ./data/full/full-',corpus,'.csv ')
-  xcommand<-paste0('grep -P \"', ids_string,'\" ./assign-1.csv')
+  xcommand<-paste0('grep -P \"', ids_string,'\" ./data/content/',corpus,'content.csv ')
  system_res <-system(xcommand,intern = TRUE)
  df<-as.data.frame(system_res, stringsAsFactors = F)
  if(corpus == "guardian"){
